c66a679f8b8e4fbc36c4e4e9ad6ac7a854d601b4
makonouchi <kenezekiel27@gmail.com>
2020-07-06 14:16:06 +0800

    update

---
+++ application/views/pages/bagyo.php
@@ -0,0 +1,109 @@
+<div id="disaster">
+    <p id="disasterTitle">Ano nga ba ang bagyo?</p>
+    <p class="descriptions">
+        Ito ay isang pagbabago sa normal na kalagayan ng panahon at karaniwang may kasamang 
+        malalakas na ihip ng hangin na may pag-ulan, pagkulog at pagkidlat. Ito ang nagiging sanhi 
+        ng pagbaha lalo na sa kapatagan, landslide o pagguho ng lupa at storm surge o daluyong 
+        sa mga tabing dagat.
+    </p>
+    <p id="disasterTitle">Mga dapat gawin bago dumating ang bagyo o tag-ulan:</p>
+    <ul class="subUl">
+        <li>
+            Kumpunihin o ipaayos ang mga sirang parte ng bahay, gaya ng alulod at bubungan.      
+        </li>
+        <li>
+            Putulin ang sanga ng mga punong malapit sa bahay.
+        </li>
+        <li>
+            Ilagay ang mga alagang hayop sa ligtas na lugar.
+        </li>
+        <li>
+            Makipag-ugnayan sa local na pamahalaan hingil sa mga anunsyo, gaya ng lugar 
+            ng evacuation center sakaling magkaroon ng paglilikas.
+        </li>
+        <li>
+            Tiyaking nakahanda ang telepono at emergency numbers para sa mga 
+            babala at tulong na kakailanganin.
+        </li>
+        <li>
+            Tiyaking may nakahandang radyo, flashlight, ekstrang baterya, gasera 
+            o kandila at posporo. Maging maingat sa paggamit ng mga ito.
+        </li>
+        
+    </ul>
+    <p id="disasterTitle">Mga dapat gawin habang bumabagyo:</p>
+    <ul class="subUl">
+        <li>
+            Makinig sa radyo, manood ng telebisyon o makibalita sa kapitbahay 
+            tungkol sa mga kaganapan sa panahon.
+        </li>
+        <li>
+            Kung ang hanapbuhay ay pangingisda, iwasan munang pumalaot.
+        </li>
+        <li>
+            Sundin ang mga paalala at babala ng barangay o pamahalaang Pambansa.   
+        </li>
+        <li>
+            Manatili sa loob ng bahay o evacuation center hanggang matapos ang bagyo.
+        </li>
+        <li>
+            Tiyaking may mga gamit pang-emergency na nasa lalagyang hindi papasukin ng tubig.
+        </li>
+        <li>
+            Mag-ingat sa paggamit ng kandila o gasera kung walang kuryente.
+        </li>
+        <li>
+            Kung kinakailagan lumikas, siguraduhing nakapatay ang kuryente, 
+            nakasara ang tanke ng gas at nakasusi ang pinto ng bahay.
+        </li>
+    </ul>
+    <p id="disasterTitle">Mga dapat gawin pagkatapos bumabagyo:</p>
+    <ul class="subUl">
+        <li>
+            Suriin ang kundisyon ng lahat ng bahagi ng tahanan.             
+        </li>
+        <li>
+            Kung pinasok ng baha ang bahay, limasin ang nakaimbak na tubig-ulan o 
+            tubig-baha at linisin ang kabahayan.
+        </li>
+        <li>
+            Siyasatin din ang mga kable ng kuryente at poste. Ipagbigay-alam agad 
+            sa kinauukulan kung may kinakailangan ayusing sa mga ito.
+        </li>
+        <li>
+            Kung nakasara ang main switch ng kuryente, tiyakin munang tuyo 
+            ang mga saksakan o outlet bago ito buksan.
+        </li>
+        <li>
+            Tulungan ang mga nangangailangan at makibahagi sa paglilinis ng komunidad o barangay.
+        </li>
+        <li>
+            Mag-ingat sa mga nabasang appliances, saksakan o lantad ng mga buhay na 
+            kurdon ng kuryente. Kung may tubig ang ano mang metal na dinadaluyan ng 
+            kuryente (conductor) ay maaring maging sanhi ng pagkakuryente.
+        </li>
+    </ul>
+    <p id="disasterTitle">Pagsagip sa taong nakuryente:</p>
+    <ul class="subUl">
+        <li>
+            Huwag direktang hahawakan ang biktima.          
+        </li>
+        <li>
+            Gumamit ng pantabig o panulak na bagay na hindi dinadaluyan ng 
+            kuryente gaya ng tuyong kahoy, gomang guwantes at iba pa.     
+        </li>
+        <li>
+            Hugutin sa saksakan o patayin ang switch na pinagmumulan ng kuryente.
+        </li>
+        <li>
+            Sikapin mailayo sa pagkakadikit ang biktima mula sa bagay na sanhi ng pagkakakuryente.
+        </li>
+        <li>
+            Siguraduhing tuyo ang iyong katawan at hindi nakaapak sa basa.
+        </li>
+        <li>
+            Pagkatapos mailayo sa peligro ang biktima , suriin kung may pagtugon o paghinga ito. 
+            Bigyan ng Cardiopulmonary Resuscitation (CPR) o paunang lunas kung kinakailangan.
+        </li>
+    </ul>
+</div>
